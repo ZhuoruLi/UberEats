aa699faa4fbf45a9b6ab036a7a6e2f11993a0470
ZhuoruLi <douglashu0822@163.com>
2023-10-07 17:45:09 -0400

    aeq

---
+++ my-app/App.js
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/*retaurant data*/}
+      <View style={styles.restaurantContainer}>
+        <Image 
+          source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg'
+          }} 
+          style = {styles.image}
+        />
+        <Text style = {styles.title}>El Cabo Coffe Bar Tres De Mayo</Text>
+        <Text style = {styles.name}>$ $1.99 15-30 minutes</Text>
+
+
+
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  restaurantContainer: {
+    width: '100%',
+    
+  },
+  image: {
+    width: '100%',
+    aspectRatio:5/3
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+});
